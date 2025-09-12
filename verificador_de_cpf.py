3c695f302abf4672561d107693294c5592c078e8
yamadapinz <146986119+yamadaguilherme@users.noreply.github.com>
2025-09-12 16:33:48 +0900

    Update and rename Verificador_de_cpf.py to verificador_de_cpf.py

--- verificador_de_cpf.py
+++ verificador_de_cpf.py
@@ -1,9 +1,10 @@
-from Verificador_input import recebe
-from Verificador_processo import processo
+from verificador_input import recebe
+from verificador_processo import processo
 
 def main():
     cpf_filtrado, verificadores = recebe()
     resultado = processo(cpf_filtrado, verificadores)
     print(resultado)
+
 
 main()
